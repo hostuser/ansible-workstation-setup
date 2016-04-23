70ae520f26ce9df31c72bf2ac95fa20b4047795d
Markus Binsteiner <makkus@gmail.com>
2016-04-24 01:13:16 +1200

    Small path fixes, added rofi-pass

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,13 @@ sudo apt install python-setuptools python-dev
 sudo easy_install pip
 sudo pip2 install ansible
 
+# ansible
+sudo mkdir /etc/ansible
+echo -e "[local]\n127.0.0.1   ansible_connection=local\n" | sudo tee /etc/ansible/hosts
+
+ansible-playbook --ask-become-pass nix_install.yml
+ansible-playbook --ask-become-pass python_install.yml
+ansible-playbook user_setup.yml
+ansible-playbook config_setup.yml
+ansible-playbook --ask-become-pass misc_setup.yml
+
