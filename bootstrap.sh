8f9a50fadefa3707f9804a176dafb20e8ea613ad
Markus Binsteiner <makkus@gmail.com>
2016-05-10 22:26:40 +1200

    Change to rule based setup.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
 # install pip
-sudo apt install python-setuptools python-dev
+sudo apt install -y python-setuptools python-dev libffi-dev libssl-dev git
 sudo easy_install pip
 sudo pip2 install ansible
 
 # ansible
+#TODO check if dir/file exists
 sudo mkdir /etc/ansible
 echo -e "[local]\n127.0.0.1   ansible_connection=local\n" | sudo tee /etc/ansible/hosts
 
 ansible-galaxy install -r requirements.yml
 
 ansible-playbook --ask-become-pass play.yml
-ansible-playbook user_setup.yml
-ansible-playbook config_setup.yml
-ansible-playbook --ask-become-pass misc_setup.yml
-
